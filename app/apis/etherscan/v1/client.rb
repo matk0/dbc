fa21da3e186639067e4ff300602f71cde4ef7ca0
Matej Lukasik <mat@journalius.com>
2022-02-18 15:02:15 -0600

    Load wallet's balance, give it a name and address.

---
+++ app/apis/etherscan/v1/client.rb
@@ -0,0 +1,38 @@
+module Etherscan
+  module V1
+    class Client
+
+      def initialize
+      end
+
+      def get_balance address: 
+        request(
+          http_method: :get,
+          endpoint: "api",
+          params: { module: "account",
+                    action: "balance",
+                    address: address,
+                    tag: "latest",
+                    apikey: Rails.application.credentials.etherscan[:api_key_token]}
+        )
+      end
+
+      private
+
+      def client
+        @_client ||= Faraday.new("https://api.etherscan.io") do |client|
+          client.request :url_encoded
+          client.adapter Faraday.default_adapter
+        end
+      end
+
+
+
+      def request(http_method:, endpoint:, params: {})
+        response = client.public_send(http_method, endpoint, params)
+        Oj.load(response.body)
+      end
+
+    end
+  end
+end
--- app/helpers/wallets_helper.rb
+++ app/helpers/wallets_helper.rb
@@ -1,2 +1,6 @@
 module WalletsHelper
+  def wallets_balance
+    client = Etherscan::V1::Client.new
+    (client.get_balance(address: @wallet.address)["result"].to_d / 1000000000000000000).round(3)
+  end
 end

cf223484f19750471de653191428b88722156b32
Matej Lukasik <mat@journalius.com>
2022-02-18 13:02:23 -0600

    Migrate wallets.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_185950) do
+ActiveRecord::Schema.define(version: 2022_02_18_190100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
+
+  create_table "wallets", force: :cascade do |t|
+    t.hstore "on_chain_properties"
+    t.string "address"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
